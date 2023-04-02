2520a526a34a4919aaada892ad4c6e0ec96327f2
Sahan Ramanayake <sahan@pastbook.com>
2023-04-02 12:24:55 +0530

    added page object test cases fo store

--- cypress/e2e/automation-test-store/add-multiple-products-to-busket.js
+++ cypress/e2e/automation-test-store/add-multiple-products-to-busket.js
@@ -1,21 +1,23 @@
+import AutoStore_HairCare_Po from "../../support/pageObject/automation-test-store/AutoStore_HairCare_PO";
+import AutoStore_Homepage_Po from "../../support/pageObject/automation-test-store/AutoStore_Homepage_PO";
 /// <reference types = "Cypress"/>
 
-before(function(){
-    cy.fixture('products').then(function(data){
-        globalThis.data=data
-    })
-})
-beforeEach(function () {
-    cy.visit("https://automationteststore.com/")
-    cy.get('a[href*="roduct/category&path"]').contains("Hair Care").click()
-})
-
-describe("Add multiple products to card", () => {
-    it("Add specific products to card", () => {
-        globalThis.data.productName.foreEach(function(name){
-            cy.addProductsToCart(name)
-        })
-
-    })
-
-})
+describe("Add multiple items to basket", () => {
+    const autoStore_Homepage_PO = new AutoStore_Homepage_Po();
+    const autoStore_HairCare_PO = new AutoStore_HairCare_Po();
+  
+    before(function () {
+      cy.fixture("products").then(function (data) {
+        globalThis.data = data;
+      });
+    });
+    beforeEach(function () {
+      autoStore_Homepage_PO.accessHomepage();
+      autoStore_Homepage_PO.clickOn_HairCare_Link();
+    });
+    
+    it("Add specific items to basket", () => {
+      autoStore_HairCare_PO.addHairCareProductsToBasket();
+    });
+  });
+  
